50245f3e03f59d37c0a45a59ea0346904015a0fb
nertti <dneretin01@gmail.com>
2023-09-15 00:13:53 +0300

    fix scale ios

--- index.js
+++ index.js
@@ -12,3 +12,8 @@ function reset() {
     document.getElementById('number1').textContent = '20';
     document.getElementById('number2').textContent = '20';
 }
+
+let buttons = document.querySelectorAll(".btn");
+buttons.forEach(function(button) {
+    console.log(button.textContent); // Пример: вывести текст на каждой кнопке
+});
